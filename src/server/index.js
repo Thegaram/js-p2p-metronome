1c1780488f8b14a398b060c80c7135e2c141ef0b
Peter Garamvolgyi <th307q@gmail.com>
2016-05-28 19:07:09 +0200

    integrate socket.io

--- src/server/index.js
+++ src/server/index.js
@@ -1,12 +1,22 @@
+import http from 'http';
 import koa from 'koa';
 import logger from 'koa-logger';
 import serve from 'koa-static';
+import socketio from 'socket.io';
 
 const port = 3000;
-const app = koa();
 
+const app = koa();
 app.use(logger());
 app.use(serve('public'));
 
-app.listen(port);
+const server = http.createServer(app.callback());
+
+const io = socketio(server);
+
+io.on('connection', (socket) => {
+    console.log('new socket.io connection');
+});
+
+server.listen(port);
 console.log(`Listening on port ${port}`);
